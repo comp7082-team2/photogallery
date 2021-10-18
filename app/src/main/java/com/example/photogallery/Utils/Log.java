d1106025bf3c32320384cdbf88f95b6fc279dcbf
Cathy Chan <ychan108@my.bcit.ca>
2021-10-18 02:05:19 -0700

    Added aspectj logging
    Co-authored-by: orangeappless <orangeappless@users.noreply.github.com>

--- app/src/main/java/com/example/photogallery/Utils/Log.java
+++
@@ -1,6 +0,0 @@
-package com.example.photogallery.Utils;
-
-
-
-public class Log {
-}
---
+++ app/src/main/java/com/example/photogallery/Utils/Logging.java
@@ -0,0 +1,527 @@
+package com.example.photogallery.Utils;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.File;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+@Aspect
+public class Logging {
+    // Models/PhotoRepository.java
+    @Before("execution(* *.findPhotos(..))")
+    public void findPhotos1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+
+        Date startTimestamp = (Date) args[0];
+        Date endTimestamp = (Date) args[1];
+        String keywords = (String) args[2];
+        String latitude = (String) args[3];
+        String longitude = (String) args[4];
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nSearch Parameters\nStart Time: ");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        if (startTimestamp != null){builderString.append(dateFormat.format(startTimestamp));}
+        else{builderString.append("null");}
+
+        builderString.append("\nEnd Time: ");
+        if (endTimestamp != null){builderString.append(dateFormat.format(endTimestamp));}
+        else{builderString.append("null");}
+
+        builderString.append("\nSearch Keyword(s): ");
+        builderString.append(keywords);
+        builderString.append("\nLatitude: ");
+        builderString.append(latitude);
+        builderString.append("\nLongitude: ");
+        builderString.append(longitude);
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.findPhotos(..))")
+    public void findPhotos2(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.loadPhotos(..))")
+    public void loadPhotos1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.loadPhotos(..))")
+    public void loadPhotos2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    // TODO: PhotoRepository: save() - Log photo argument?
+    @Before("execution (* *.save(..))")
+    public void save1(JoinPoint joinPoint){
+        // Object[] args = joinPoint.getArgs();
+        // String photo = (String) args[0];
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        // builderString.append("\nAdding photo: ");
+        // builderString.append(photo);
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.save(..))")
+    public void save2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    // Models/Photo.java
+    // TODO: Photo: Photo
+
+    // TODO: Photo: getPhotoFile() - display File return?
+    @Before("execution(* *.getPhotoFile(..))")
+    public void getPhotoFile1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+    
+    @AfterReturning(pointcut="execution(* *.getPhotoFile(..))", returning="photoFile")
+    public void getPhotoFile2(JoinPoint joinPoint, File photoFile){
+        // Object[] args = joinPoint.getArgs();
+        // String photo = (String) args[0];
+        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        // builderString.append("\nReturn Photo File: ");
+        // builderString.append(photoFile);
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.getCaption(..))")
+    public void getCaption1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @AfterReturning(pointcut="execution(* *.getCaption(..))", returning="caption")
+    public void getCaption2(JoinPoint joinPoint, String caption) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nReturn Caption: ");
+        builderString.append(caption);
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+
+    @Before("execution(* *.updateCaption(..))")
+    public void updateCaption1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        String caption = (String) args[0];
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nNew Caption: ");
+        builderString.append(caption);
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.updateCaption(..))")
+    public void updateCaption2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.getDatestamp(..))")
+    public void getDatestamp1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }   
+
+    @AfterReturning(pointcut="execution(* *.getDatestamp(..))", returning="dateString")
+    public void getDatestamp2(JoinPoint joinPoint, String dateString) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nReturn Date Stamp: ");
+        builderString.append(dateString);
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+
+    @Before("execution(* *.getBitmap(..))")
+    public void getBitmap1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    // TODO: getBitmap() - Express bitmap in Log.d?
+    @AfterReturning(pointcut="execution(* *.getBitmap(..))", returning="bitmap")
+    public void getDatestamp2(JoinPoint joinPoint, BitmapFactory bitmap) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        // builderString.append("\nReturn Date Stamp: ");
+        // builderString.append(dateString);
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+
+    @Before("execution(* *.getDate(..))")
+    public void getDate1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @AfterReturning(pointcut="execution(* *.getDate(..))", returning="date")
+    public void getDate2(JoinPoint joinPoint, Date date) {        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nReturn Date: ");
+        if (date==null){builderString.append("null");}
+        else{builderString.append(date);}
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+
+    // Utils/ExifHelper.java
+    @Before("execution(* *.convert(..))")
+    public void convert1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        double lat_long = (double) args[0];
+        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nDecimal to DMS Conversion Parameters (No Reference)\nLatitude/Longitude in Decimal:");
+        builderString.append(lat_long);
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @AfterReturning(pointcut="execution(* *.convert(..))", returning="DMS")
+    public void convert2(JoinPoint joinPoint, String DMS) {        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nReturn Latitude/Longitude in DMS (No Reference): ");
+        builderString.append(DMS);
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+
+    @Before("execution(* *.latitudeRef(..))")
+    public void latitudeRef1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        double latitude = (double) args[0];
+        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nLatitude Reference Converter Parameters\nLatitude:");
+        builderString.append(latitude);
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @AfterReturning(pointcut="execution(* *.latitudeRef(..))", returning="latitudeRef")
+    public void latitudeRef2(JoinPoint joinPoint, String latitudeRef) {        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nReturn Latitude Reference: ");
+        builderString.append(latitudeRef);
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+
+    @Before("execution(* *.longitudeRef(..))")
+    public void longitudeRef1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        double longitude = (double) args[0];
+        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nLongitude Reference Converter Parameters\nLongitude:");
+        builderString.append(longitude);
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @AfterReturning(pointcut="execution(* *.longitudeRef(..))", returning="longitudeRef")
+    public void longitudeRef2(JoinPoint joinPoint, String longitudeRef) {        
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        builderString.append("\nReturn Longitude Reference: ");
+        builderString.append(longitudeRef);
+
+        Log.d("Finish Call:", builderString.toString());
+    }
+    
+    // GalleryPresenter.java
+    @Before("execution(* *.startLocationUpdates(..))")
+    public void startLocationUpdates1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.startLocationUpdates(..))")
+    public void startLocationUpdates2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.stopLocationUpdates(..))")
+    public void stopLocationUpdates1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.stopLocationUpdates(..))")
+    public void stopLocationUpdates2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.setExif(..))")
+    public void setExif1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        String path = (String) args[0];
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nSetting EXIF at: ");
+        builderString.append(path);
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.setExif(..))")
+    public void setExif2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    // TODO: takePhoto() - display parameters?
+    @Before("execution(* *.takePhoto(..))")
+    public void takePhoto1(JoinPoint joinPoint) {
+        // Object[] args = joinPoint.getArgs();
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.takePhoto(..))")
+    public void takePhoto2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }    
+
+    @Before("execution(* *.openSearch(..))")
+    public void openSearch1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.openSearch(..))")
+    public void openSearch2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    // TODO: calls both MainActivity and GalleryPresenter
+    @Before("execution(* *.displayPhoto(..))")
+    public void displayPhoto1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.displayPhoto(..))")
+    public void displayPhoto2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.createImageFile(..))")
+    public void createImageFile1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.createImageFile(..))")
+    public void createImageFile2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.onActivityResult(..))")
+    public void onActivityResult1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        int requestCode = (int) args[0];
+        int resultCode = (int) args[1];
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nrequestCode: ");
+        builderString.append(requestCode);
+        builderString.append("\nresultCode: ");
+        builderString.append(resultCode);
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.onActivityResult(..))")
+    public void onActivityResult2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    } 
+    
+    @Before("execution(* *.sharePhoto(..))")
+    public void sharePhoto1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.sharePhoto(..))")
+    public void sharePhoto2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+    
+    @Before("execution(* com.example.photogallery.Presenters.GalleryPresenter.scrollPhotos(..))")
+    public void scrollPhotos1(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        boolean isNext = (boolean) args[0];
+        String caption = (String) args[1];
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        builderString.append("\nisNext: ");
+        builderString.append(isNext);
+        builderString.append("\ncaption: ");
+        builderString.append(caption);
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* com.example.photogallery.Presenters.GalleryPresenter.scrollPhotos(..))")
+    public void scrollPhotos2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    // SearchActivity.java
+    @Before("execution(* *.onCreate(..))")
+    public void onCreate1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.onCreate(..))")
+    public void onCreate2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.cancel(..))")
+    public void cancel1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.cancel(..))")
+    public void cancel2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+
+    @Before("execution(* *.go(..))")
+    public void go1(JoinPoint joinPoint) {
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+        
+        Log.d("Start Call: ", builderString.toString());
+    }
+
+    @After("execution(* *.go(..))")
+    public void go2(JoinPoint joinPoint){
+        StringBuilder builderString = new StringBuilder();
+        builderString.append(joinPoint.toLongString());
+
+        Log.d("Finish Call: ", builderString.toString());
+    }
+}
